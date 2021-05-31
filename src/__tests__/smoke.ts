0dce7cda1f4c806520caed7440bee8eab0e42dd2
Anoop Simon <s451421990@gmail.com>
2021-05-31 20:40:06 +1000

    added test data capture from json file

--- src/__tests__/smoke.ts
+++ src/__tests__/smoke.ts
@@ -4,11 +4,11 @@ import {TestData } from "../core/TestData";
 
 /**To verify Search functionality in Bing. */
 it("Verify Bing Search", async () => 
-{
-  
+{  
     var searchTerm = new TestData().search().searchTerms[0];
+    var appUrl='https://www.bing.com/';
     const searchPage = new SearchPage(page);
-    await searchPage.navigate('https://www.bing.com/');
+    await searchPage.navigate(appUrl);
     await searchPage.search(searchTerm);
     await searchPage.verifyResult();
 
