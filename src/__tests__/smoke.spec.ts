8529a86b39cfdfd8f02085586ef204a78121c729
Anoop Simon <anoop.simon@adudev.com>
2022-04-21 10:40:17 +1000

    added test cases back

--- src/__tests__/smoke.spec.ts
+++ src/__tests__/smoke.spec.ts
@@ -8,9 +8,9 @@ function getPage<T>(type: { new (args:any): T }, args): T {
 /**To verify Search functionality in Bing. */
 it.each(search.searchTermCoding)("Verify Bing Search", async (data) => 
 {  
-//     const searchPage = getPage(SearchPage,page);
-//     var appUrl='https://www.bing.com/';
-//     await searchPage.navigate(appUrl);
-//     await searchPage.search(data);
-//    await searchPage.verifyResult();
+    const searchPage = getPage(SearchPage,page);
+    var appUrl='https://www.bing.com/';
+    await searchPage.navigate(appUrl);
+    await searchPage.search(data);
+   await searchPage.verifyResult();
 });
